d7d6ca078a96de9441d575c4419220d6a86e4025
Alex Stelea <alex@stelea.io>
2021-04-30 19:04:58 +0200

    feat: add dotenv

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Radix Node - EC2
 
+Set following values in `.env`
+
+```
+CDK_DEFAULT_ACCOUNT=XXX
+CDK_DEFAULT_REGION=eu-west-1
+VPC_ID=vpc-XXX
+SSH_KEY_NAME=XXX
+```
+
 ## Useful commands
 
-- `npm run deploy` deploy this stack to your default AWS account/region
+- `npx cdk deploy` deploy this stack to your default AWS account/region
--- lib/radix-node-stack.ts
+++ lib/radix-node-stack.ts
@@ -1,22 +1,35 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+
+require('dotenv').config();
+
+const config = {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+};
 
 export class RadixNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, { ...props, ...config });
 
-    const vpc = new ec2.Vpc(this, 'vpc', { maxAzs: 1 });
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true });
 
     const securityGroup = new ec2.SecurityGroup(
       this,
       'radixNodeSecurityGroup',
       {
         vpc,
-        securityGroupName: 'radix-node-sg',
-        description: 'Radix node security group',
+        securityGroupName: 'radix-node-security-group',
         allowAllOutbound: true,
       }
     );
+
+    const role = new iam.Role(this, 'radixNodeRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
 
     securityGroup.addIngressRule(
       ec2.Peer.anyIpv4(),
@@ -48,7 +61,8 @@ export class RadixNodeStack extends cdk.Stack {
 
     const instance = new ec2.Instance(this, 'radixNodeEc2Instance', {
       instanceName: 'radixNode',
-      keyName: 'ec2',
+      keyName: process.env.SSH_KEY_NAME,
+      role: role,
       vpc,
       securityGroup,
       instanceType: ec2.InstanceType.of(
